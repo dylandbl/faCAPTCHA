5b57c6d0e4c1b35f9574cce5c246646e0c405850
DylanBlair <dy.blair@gmail.com>
2022-06-29 18:34:30 -0600

    fix: removed notARobotText from example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -10,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <FaCaptcha
-        notARobotText="Dylan"
         onVerificationComplete={handleVerification}
         imgTopicUrls={imagesArr1}
       />
