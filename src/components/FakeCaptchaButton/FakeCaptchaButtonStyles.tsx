f6c480b65560dbd84799b5e416585f230ae52908
DylanBlair <dy.blair@gmail.com>
2022-07-01 15:43:12 -0600

    styles: increased button height

--- src/components/FakeCaptchaButton/FakeCaptchaButtonStyles.tsx
+++ src/components/FakeCaptchaButton/FakeCaptchaButtonStyles.tsx
@@ -3,7 +3,7 @@ import styled from "@emotion/styled";
 export const CaptchaButton = styled.div`
   min-width: 275px;
   max-width: 275px;
-  height: 48px;
+  height: 52px;
   border-radius: 2px;
   border: 1px solid lightgrey;
   padding: 10px;
