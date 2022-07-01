3d67fcc47c3833eea35b379411eda11505ff9933
DylanBlair <dy.blair@gmail.com>
2022-07-01 13:33:57 -0600

    feat: added new imagesArr for PhotoShoot

--- example/src/exampleData/imageUrls.ts
+++ example/src/exampleData/imageUrls.ts
@@ -93,16 +93,43 @@ export const demo1 = [
 ];
 
 export const imagesArr2 = [
-  "https://www.publicdomainpictures.net/pictures/340000/velka/freshwater-fish-food-1588844402K9q.jpg",
-  "https://as2.ftcdn.net/v2/jpg/00/24/44/65/1000_F_24446556_CDgGuARl9dTSJXUzQJvgjjfLHEGKKvJM.jpg",
-  "https://as1.ftcdn.net/v2/jpg/01/49/66/96/1000_F_149669654_w9lPVtw4dGpnjVoG9jAWpPkjiJuDTUIG.jpg",
-  "https://www.publicdomainpictures.net/pictures/180000/velka/clown-loaches.jpg",
-  "https://www.publicdomainpictures.net/pictures/280000/velka/clown-monsters-of-halloween.jpg",
-  "https://as1.ftcdn.net/v2/jpg/02/37/02/28/1000_F_237022893_CbNuElBdTGRz6596zDkIdiRL9Yz24i16.jpg",
-  "https://www.publicdomainpictures.net/pictures/420000/velka/vintage-weihnachten-postkarte-alt-1635350707Dn6.jpg",
-  "https://as2.ftcdn.net/v2/jpg/00/02/50/89/1000_F_2508984_wXm1l9O4JyrsGIxzNO0k8N2mfCDk8s.jpg",
-  "https://as1.ftcdn.net/v2/jpg/01/74/39/48/1000_F_174394832_M0IIWRbGnujltXis88MShKpHanslEsiR.jpg",
-  "https://www.publicdomainpictures.net/pictures/450000/velka/mermaid-vintage-art.jpg",
+  {
+    url: "https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvam9iNjc5LTA0NS14LmpwZw.jpg",
+    topics: ["dogs", "a dog", "illustrations", "art"],
+  },
+  {
+    url: "https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvam9iNjc5LTA3MS14LmpwZw.jpg",
+    topics: ["rooster", "chicken", "birds", "illustrations", "art"],
+  },
+  {
+    url: "https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQyMzUtcGRzdHViYnMwMDEyNi1pbWFnZV8zLmpwZw.jpg",
+    topics: ["chicken", "birds", "illustrations", "art"],
+  },
+  {
+    url: "https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQyMzUtcGRzdHViYnMwMDExNC1pbWFnZTAxXzEuanBn.jpg",
+    topics: ["horse", "skeleton", "illustrations", "art"],
+  },
+  {
+    url: "https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGRmbWE0LXBkamVhbmJlcm5hcmQwMDAyOC1pbWFnZS5qcGc.jpg",
+    topics: ["cats", "a cat", "illustrations", "art"],
+  },
+  {
+    url: "https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvcGQyMDQtNDQuanBn.jpg",
+    topics: ["donkey", "an ass", "asses", "illustrations", "art"],
+  },
+  {
+    url: "https://images.rawpixel.com/image_1000/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA1L3BkMjEtNTEtMDItcm9iLmpwZw.jpg",
+    topics: ["dogs", "a dog", "illustrations", "art"],
+  },
+  {
+    url: "https://images.rawpixel.com/image_1000/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvam9iNjc5LTAzNS14LmpwZw.jpg",
+    topics: ["cats", "a cat", "illustrations", "art"],
+  },
+
+  {
+    url: "https://images.rawpixel.com/image_1000/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDIyLTA0L2pvYjcxMi0xODQtdi5qcGc.jpg",
+    topics: ["pigs", "a pig", "illustrations", "art"],
+  },
 ];
 
 export default imagesArr1;
