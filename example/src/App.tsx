55a15e48582d576024621d970f86df2e3711ce87
DylanBlair <dy.blair@gmail.com>
2022-07-05 18:13:52 -0600

    refactor(exmaple)!: removed screenWidth from App.tsx and <Header />

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,11 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { ShowRoom } from "./sections/ShowRoom";
-import { useWindowSize } from "./utils/hooks";
 
 function App() {
-  const screenWidth = useWindowSize();
-
   return (
     <div className="App">
-      <Header screenWidth={screenWidth} />
+      <Header />
 
       <ShowRoom />
 
